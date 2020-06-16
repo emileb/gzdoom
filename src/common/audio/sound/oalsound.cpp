50066434e53269d0e1bbd5dd446b048e4de3fc11
Emile Belanger <emile.belanger@gmail.com>
2020-06-16 20:04:00 +0100

    Make stat.h hack only for 32bit arm

--- src/common/audio/sound/oalsound.cpp
+++ src/common/audio/sound/oalsound.cpp
@@ -1072,7 +1072,7 @@ SoundHandle OpenALSoundRenderer::LoadSound(uint8_t *sfxdata, int length)
 	SoundDecoder_GetInfo(decoder, &srate, &chans, &type);
 	int samplesize = 1;
 #ifdef __MOBILE__
-	if (chans == ChannelConfig_Mono|| monoize)
+	if (chans == ChannelConfig_Mono || monoize)
 #else
 	if (chans == ChannelConfig_Mono)
 #endif
