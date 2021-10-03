fc718e16963aeed333599781b41eed5a230637cb
Emile Belanger <emile.belanger@gmail.com>
2021-10-03 10:53:21 +0100

    Allow SW pallet mode for work on GLES3 devices

--- src/common/rendering/gles/gles_hwtexture.cpp
+++ src/common/rendering/gles/gles_hwtexture.cpp
@@ -132,11 +132,18 @@ unsigned int FHardwareTexture::CreateTexture(unsigned char * buffer, int w, int 
 
 
 #if USE_GLES2
+	sourcetype = GL_BGRA;
+	texformat = GL_BGRA;
+
+	// Only GLES3 device will work with palleted SW mode
+#define GL_RED 0x1903
+#define GL_R8 0x8229
+
 	if (glTextureBytes == 1)
 	{
 		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-		sourcetype = GL_ALPHA;
-		texformat = GL_ALPHA;
+		sourcetype = GL_RED;
+		texformat = GL_R8;
 	}
 	else
 	{
