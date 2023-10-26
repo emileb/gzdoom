8faf7059b0f25bf3e9127ecfb9dd98314d6317f1
Emile Belanger <emile.belanger@gmail.com>
2023-10-26 20:39:57 +0100

    Don't save patch_fluidsynth

--- src/common/audio/music/music_config.cpp
+++ src/common/audio/music/music_config.cpp
@@ -117,7 +117,7 @@ CUSTOM_CVAR(String, fluid_lib, "", CVAR_ARCHIVE | CVAR_GLOBALCONFIG | CVAR_VIRTU
 }
 
 #ifdef __ANDROID__
-CUSTOM_CVAR(String, fluid_patchset,  "./audiopack/snd_fluidsynth/fluidsynth.sf2", CVAR_ARCHIVE | CVAR_GLOBALCONFIG | CVAR_VIRTUAL)
+CUSTOM_CVAR(String, fluid_patchset,  "./audiopack/snd_fluidsynth/fluidsynth.sf2", 0)
 #else
 CUSTOM_CVAR(String, fluid_patchset, GAMENAMELOWERCASE, CVAR_ARCHIVE | CVAR_GLOBALCONFIG | CVAR_VIRTUAL)
 #endif
