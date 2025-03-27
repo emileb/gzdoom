f48cfc4aac9e11fa0566a6d2abc0eda0e78d3f0d
Emile Belanger <emile.belanger@gmail.com>
2025-03-27 18:43:31 +0000

    Squash mobile changes

--- src/common/objects/autosegs.h
+++ src/common/objects/autosegs.h
@@ -116,7 +116,7 @@ public:
 
 		for (void **it = begin; it < end; ++it)
 		{
-			if (intptr_t(it) > 0xffff && *it && intptr_t(*it) > 0xffff)
+			if (uintptr_t(it) > 0xffff && *it && uintptr_t(*it) > 0xffff)
 			{
 				func(reinterpret_cast<ArgType>(*it));
 			}
@@ -131,7 +131,7 @@ public:
 
 		for (void **it = begin; it < end; ++it)
 		{
-			if (intptr_t(it) > 0xffff && *it && intptr_t(*it) > 0xffff)
+			if (uintptr_t(it) > 0xffff && *it && uintptr_t(*it) > 0xffff)
 			{
 				if (!func(reinterpret_cast<ArgType>(*it)))
 				{
