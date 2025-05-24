db2808d7910434f2af2b04505a28c4de6af7df1d
Emile Belanger <emile.belanger@gmail.com>
2025-05-24 16:08:12 +0100

    Fix for NDK24, fix for Linux

--- src/common/objects/autosegs.cpp
+++ src/common/objects/autosegs.cpp
@@ -62,7 +62,7 @@
 #define AUTOSEG_START(name) __start_##name
 #define AUTOSEG_STOP(name) __stop_##name
 #define AUTOSEG_VARIABLE(name, autoseg) \
-	void* name##DummyPointer __attribute__((section(AUTOSEG_STR(autoseg)))) __attribute__((used)); \
+	void* name##DummyPointer __attribute__((section(AUTOSEG_STR(autoseg)))) __attribute__((used)) __attribute__((visibility("default"))); \
 	extern void* AUTOSEG_START(autoseg); \
 	extern void* AUTOSEG_STOP(autoseg); \
 	namespace AutoSegs { FAutoSeg name{ &AUTOSEG_START(autoseg), &AUTOSEG_STOP(autoseg) }; }
