186d134bf2c4d0246513c85f29b330b2af53fe40
Emile Belanger <emile.belanger@gmail.com>
2026-08-30 15:26:32 +0100

    Move spriv path

--- mobile/Android_src.mk
+++ mobile/Android_src.mk
@@ -91,7 +91,6 @@ LOCAL_C_INCLUDES := \
         $(GZDOOM_TOP_PATH)/libraries/discordrpc/include \
         $(GZDOOM_TOP_PATH)/libraries/ZVulkan/include \
         $(GZDOOM_TOP_PATH)/libraries/ZVulkan/src \
-        $(TOP_DIR)/Doom/spirv-cross \
 \
  $(SDL_INCLUDE_PATHS) \
  $(TOP_DIR)/AudioLibs_OpenTouch/openal-soft/src/main/jni/openal/include/AL \
